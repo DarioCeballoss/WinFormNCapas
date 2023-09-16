c97cc38c53a36db62d1b1fd58fc77ce07a8b546b
Dario Ceballos <dario_tec_aut@hotmail.com.ar>
2023-09-15 23:02:38 -0300

    3 capas - Conexion a datos

---
+++ NcapasBasico/Datos/CD_Conexion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient; //agregamos 
+using System.Data; //agregamos 
+
+namespace Datos
+{
+    class CD_Conexion
+    {
+        //Cadena conexion
+        private SqlConnection Conexion = new SqlConnection("Server=DESKTOP-UEPK13H\\RONETJOHN;DataBase= Practica;Integrated Security=true");
+        //Abre conexion
+        public SqlConnection AbrirConexion()
+        {
+            if (Conexion.State == ConnectionState.Closed)
+                Conexion.Open();
+            return Conexion;
+        }
+        //Cierra conexion
+        public SqlConnection CerrarConexion()
+        {
+            if (Conexion.State == ConnectionState.Open)
+                Conexion.Close();
+            return Conexion;
+        }
+    }
+}
