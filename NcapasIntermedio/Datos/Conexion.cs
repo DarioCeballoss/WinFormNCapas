28379a1ffa5a194a450b55e61310debe60af18ba
Dario Ceballos <dario_tec_aut@hotmail.com.ar>
2023-09-20 15:11:06 -0300

    commit

--- NcapasIntermedio/Datos/Conexion.cs
+++ NcapasIntermedio/Datos/Conexion.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data.SqlClient;
 
 namespace Datos
 {
-    class Conexion
+    public abstract class Conexion
     {
+        private readonly string connectionString;
+
+        public ConnectionToSql(){
+            connectionString = "Server=DESKTOP-6U8Q54U\\SQLEXPRESS;DataBase= Practica;Integrated Security=true";
+        }
+
+        protected SqlConnection GetConnection(){
+            return new SqlConnection(connectionString);
+        }
     }
 }
---
+++ NcapasIntermedio/Datos/UserDao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace Datos
+{
+    class UserDao:Conexion
+    {
+        public bool Login(string user, string pass)
+        { 
+        
+        }
+    }
+}
